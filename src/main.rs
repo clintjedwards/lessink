7e15662dd8fede997236b19609c8c3902473f83c
Clint J Edwards <clint.j.edwards@gmail.com>
2025-04-12 13:19:28 -0400

    more skeleton

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod parser;
 mod renderer;
 
 use clap::{Parser, Subcommand};
+use walkdir::WalkDir;
 
 #[derive(Debug, Parser, Clone)]
 #[command(name = "lessink")]
@@ -36,12 +37,69 @@ fn main() {
 
 // We walk the input file system and file by file we create the new html file in the output location.
 fn build(input: std::path::PathBuf, output: std::path::PathBuf) {
-    let unparsed_file = std::fs::read_to_string(input).unwrap();
-
-    let ast = parser::parse(&unparsed_file).unwrap();
-    let html = renderer::Html::render(ast);
-    print!("{}", html);
+    walk_and_render(&input, &output);
 }
 
 // This is build followed by a rustembed into a webserver so the user can see what we created.
 fn run() {}
+
+struct Structure {
+    kind: StructureKind,
+    inner: Vec<Structure>,
+}
+
+enum StructureKind {
+    Section(String),
+    NewLevelStart,
+    File(String, String), // file name -> file path
+}
+
+fn read_settings(input_dir: &std::path::Path) -> Vec<(String, String)> {
+    let mut settings_file_path = input_dir.to_path_buf();
+    settings_file_path.push("lessink_settings.lessink");
+
+    let settings_file = std::fs::read_to_string(settings_file_path).unwrap();
+
+    let ast = parser::parse(&settings_file).unwrap();
+    let mut structure = vec![];
+
+    for node in ast {
+        match node.as_rule() {
+            parser::Rule::h1 => {
+                let text = node.into_inner().next().unwrap().as_span().as_str();
+                structure.push(StructureKind::Section(text.to_string()));
+            }
+            parser::Rule::link => {}
+            parser::Rule::list => {}
+            _ => continue,
+        }
+    }
+
+    todo!();
+}
+
+fn walk_and_render(input: &std::path::Path, output: &std::path::Path) {
+    for entry in WalkDir::new(input) {
+        let entry = entry.unwrap();
+        let path = entry.path();
+
+        let rel_path = path.strip_prefix(input).unwrap();
+        let out_path = output.join(rel_path);
+
+        if path.is_dir() {
+            std::fs::create_dir_all(&out_path).unwrap();
+        } else if path.is_file() {
+            if path.file_name().unwrap().to_string_lossy() == "lessink_settings.lessink" {
+                continue;
+            }
+
+            let unparsed_file = std::fs::read_to_string(path).expect("test");
+
+            let ast = parser::parse(&unparsed_file).unwrap();
+            let html = renderer::Html::render(ast);
+
+            std::fs::create_dir_all(out_path.parent().unwrap()).unwrap();
+            std::fs::write(&out_path, html).unwrap();
+        }
+    }
+}
--- src/parser.rs
+++ src/parser.rs
@@ -14,23 +14,32 @@ pub fn parse(input: &str) -> Result<Pairs<'_, Rule>, pest::error::Error<Rule>> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use once_cell::unsync::Lazy;
-    use std::cell::RefCell;
-
-    thread_local! {
-        static AST: Lazy<RefCell<Pairs<'static, Rule>>> = Lazy::new(|| {
-            let unparsed_file = std::fs::read_to_string("test.lessink").unwrap();
-            let unparsed_file = Box::leak(Box::new(unparsed_file));
-
-            RefCell::new(parse(unparsed_file).unwrap())
-        });
-    }
 
     #[test]
     fn test_h1() {
-        AST.with(|ast| {
-            let next = ast.borrow_mut().next().unwrap();
-            assert_eq!(next.as_rule(), Rule::h1);
-        });
+        let input = "# Heading One";
+
+        let mut ast = parse(input).unwrap();
+
+        let output = ast.next().unwrap();
+
+        assert_eq!(output.as_rule(), Rule::h1);
+        assert_eq!(output.as_str(), "# Heading One");
+    }
+
+    #[test]
+    fn test_list_simple() {
+        let input = r#"
+- Bullet Point One
+  - Bullet Point Inner
+- Bullet Point Two
+"#;
+
+        let mut ast = parse(input).unwrap();
+        dbg!(&ast);
+
+        let output = ast.next().unwrap();
+
+        assert_eq!(output.as_rule(), Rule::list);
     }
 }
